0c4696a2732722dc15bcce9288103ab58656ec4d
brunabdesouza <bruna.bdesouza@gmail.com>
2021-03-28 17:36:22 +1100

    deleted unused code

--- app/controllers/shelters_controller.rb
+++ app/controllers/shelters_controller.rb
@@ -18,17 +18,13 @@ class SheltersController < ApplicationController
 
   def search
 
-    # search = Shelter.near([
-    #   params[:latitude],
-    #   params[:longitude]
-    # ], 50, units: :km )
     search = Shelter.near(
       params[:location],
       params[:radius].to_i, units: :km
      )
-     # .joins lets me get all the pets that belongs to the shelter I found in the search
+     # .joins lets me get all the pets that belongs to the shelter I found in the search by location.
       .joins(:pets)
-      # .select is how I get the fields from the pets association and get them to show on my query string. Also have changed the field name so they don't clash with the shelter fields
+      # .select is how I get the fields from the pets association and get them to show on my query string. Also have changed the field name so they don't clash with the shelter field names.
       .select('shelters.name as shelter_name, pets.id as pet_id, pets.name as pet_name, pets.description as pet_description, pets.animal_type as pet_type, pets.image as pet_image')
       # .where is what I get from my search in the frontend
       .where(pets: { animal_type: params[:animal_type], available: true}
@@ -38,10 +34,6 @@ class SheltersController < ApplicationController
 
     render json: search
 
-    # search = Shelter.near([
-    #   params[:latitude],
-    #   params[:longitude]
-    # ], 50, units: :km )
 
   end # search
 
